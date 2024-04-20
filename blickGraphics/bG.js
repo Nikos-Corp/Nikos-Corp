032b69f24587eb9bdb342c604cbcf584700d1c78
Nikos-Corp <166372209+Nikos-Corp@users.noreply.github.com>
2024-04-20 15:27:31 +0200

    toggle

--- blickGraphics/bG.js
+++ blickGraphics/bG.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navbarToggle = document.getElementById('navbarToggle');
+  const navbarMenu = document.getElementById('navbarMenu');
 
+  navbarToggle.addEventListener('click', function() {
+    if (navbarMenu.style.display === 'none' || navbarMenu.style.display === '') {
+      navbarMenu.style.display = 'flex';
+    } else {
+      navbarMenu.style.display = 'none';
+    }
+  });
+});
