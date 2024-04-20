f557ec41000bab03dbd79eb88bece10a94196676
Nikos-Corp <166372209+Nikos-Corp@users.noreply.github.com>
2024-04-20 19:06:10 +0200

    add colaps

--- blickGraphics/bG.js
+++ blickGraphics/bG.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   const navbarToggle = document.getElementById('navbarToggle');
-  const navbarMenu = document.getElementById('navbarMenu');
+  const menuItems = document.querySelectorAll('.menuItem');
 
   navbarToggle.addEventListener('click', function() {
-    if (navbarMenu.style.display === 'none' || navbarMenu.style.display === '') {
-      navbarMenu.style.display = 'flex';
-    } else {
-      navbarMenu.style.display = 'none';
-    }
+    menuItems.forEach(function(item) {
+      if (item.style.display === 'none' || item.style.display === '') {
+        item.style.display = 'block';
+        setTimeout(function() {
+          item.style.opacity = '1';
+        }, 10); // Delay added for transition to work smoothly
+      } else {
+        item.style.opacity = '0';
+        setTimeout(function() {
+          item.style.display = 'none';
+        }, 300); // Adjust as needed to match CSS transition duration
+      }
+    });
   });
 });
