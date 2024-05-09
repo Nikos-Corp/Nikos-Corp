4388e9450093f74f856715d92b33186b336dc92f
Nikos-Corp <166372209+Nikos-Corp@users.noreply.github.com>
2024-05-09 15:14:26 +0200

    tooggle debug

--- blickGraphics/bG.js
+++ blickGraphics/bG.js
@@ -1,24 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   const navbarToggle = document.getElementById('navbarToggle');
-  
-  const menuItems = document.querySelectorAll('.menuItem');
-  
+  const darkDiv = document.querySelector('.invisibleDiv');
+  const menuItems = document.querySelectorAll('.invisibleMenuItem');
+
   navbarToggle.addEventListener('click', function() {
-
+    // Loop through each menuItem element
     menuItems.forEach(function(item) {
-      if (item.style.display === 'none' || item.style.display === '') {
-        item.style.display = 'block';
-        setTimeout(function() {
-          item.style.opacity = '1';
-        }, 10); 
-      } else {
-        item.style.opacity = '0';
-        setTimeout(function() {
-          item.style.display = 'none';
-        }, 300); // Adjust as needed to match CSS transition duration
-      }
+      // Toggle the 'invisibleMenuItem' class on the element
+      item.classList.toggle('menuItem');
+      darkDiv.classList.toggle('darkDiv')
     });
-
   });
-  
 });
